88fbafebac7ebdc27b76822c9744cf8d3bfb71e3
hollarves <henryollh205@gmail.com>
2017-06-06 08:03:48 +0200

    Adding switch statement for webhooks

--- routes/webhooks.ts
+++ routes/webhooks.ts
@@ -40,9 +40,56 @@ export class WebHooks extends Paypal {
         console.log("Creating Router for [Webook::billing_plan_webhooks]")
         router.post('/subscription', (req: Request, res: Response) => {
             console.log("Webhook event!")
-            console.log(req.body.event_type)
-            console.log(req.body.summary)
-            console.log(req.body.resource.name)
+            switch (req.body.event_type) {
+                case "BILLING.PLAN.CREATED":
+                    console.log("Billing plan created!")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                case "BILLING.PLAN.UPDATED":
+                    console.log("Billing plan updated!")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                case "BILLING.SUBSCRIPTION.CANCELLED":
+                    console.log("Billing plan cancelled!")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                case "BILLING.SUBSCRIPTION.CREATED":
+                    console.log("Billing subscription created!")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                case "BILLING.SUBSCRIPTION.RE-ACTIVATED":
+                    console.log("Billing subscription reactivated")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                case "BILLING.SUBSCRIPTION.SUSPENDED":
+                    console.log("Billing subscription suspended!")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                case "BILLING.SUBSCRIPTION.UPDATED":
+                    console.log("Billing subscription updated!")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+                default:
+                    console.log("Some unknown webhook")
+                    console.log(req.body.event_type)
+                    console.log(req.body.summary)
+                    console.log(req.body.resource.name)
+                    break
+            }
         })
     }
 }
--- server.ts
+++ server.ts
@@ -15,40 +15,40 @@ export class Server {
     constructor() {
         this.app = Express()
         this.app.use(BodyParser.json())
-        this.config()
         this.webhooks()
         this.api()
     }
 
     public api() {
         let router = Express.Router()
-        //Initializing billing_agreement for paypal
+        // Initializing billing_agreement for paypal
         Paypal.billing_agreement(router)
-        //Initializing billing_agreement_list for paypal
+        // Initializing billing_agreement_list for paypal
         Paypal.billing_agreement_list(router)
-        //Intializing billing_agreement_execute for paypal
+        // Intializing billing_agreement_execute for paypal
         Paypal.billing_agreement_execute(router)
-        //Initializing billing_agreement_cancel for paypal
+        // Initializing billing_agreement_cancel for paypal
         Paypal.billing_agreement_cancel(router)
-        //Initializing billing_agreement_suspend for paypal
+        // Initializing billing_agreement_suspend for paypal
         Paypal.billing_agreement_suspend(router)
-        //Initializing payment_create for paypal
+        // Initializing payment_create for paypal
         Paypal.payment_create(router)
-        //Initializing payment_execute for paypal
+        // Initializing payment_execute for paypal
         Paypal.payment_execute(router)
-        //Using router
+        // Exposing endpoint
         this.app.use('/paypal', router)
     }
 
     public webhooks() {
+        // Instantiating webhook class
         let webhooks = new WebHooks
+        // Creating router for webooks
         let router = Express.Router()
         webhooks.billing_plans(router)
+        // Exposing endpoint
         this.app.use('/webhooks', router)
 
     }
-
-    public config() { return false }
 }
 
 const server = Server.bootstrap().app
